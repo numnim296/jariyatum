eca77a01717a83acef7cca3c4dfd29f9679e07aa
numnim296 <muthita.mu.61@ubu.ac.th>
2021-02-23 14:15:43 +0700

    happyImage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import happyIcon from './happy.png';
 import './App.css';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>WINTER OF AESPA</h1>
+        <img src={happyIcon}/>
         <h4>numnim muthita munmueang</h4>
       </header>
     </div>
